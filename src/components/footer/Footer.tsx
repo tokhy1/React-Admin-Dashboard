243f11ed73f19b1f7fce6bf80543b638263616a0
mohamed21346587 <mohamed21346587@gmail.com>
2024-08-09 22:13:06 +0000

    Finished Footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,12 @@
 import "./footer.scss";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <span>Mohamed Ashraf</span>
+      <span>©React Admin Dashboard</span>
+    </div>
+  );
 };
 
 export default Footer;
