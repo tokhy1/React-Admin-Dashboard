f81474725c006eb66f2f75eaf34fc3bcaba63e00
mohamed21346587 <mohamed21346587@gmail.com>
2024-08-09 21:09:51 +0000

    setup layout and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import Home from "./pages/Home/Home";
+import Products from "./pages/Products/Products";
+import Users from "./pages/Users/Users";
 
 function App() {
   return (
     <div>
       <Home />
+      <Users />
+      <Products />
     </div>
   );
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,7 +2,7 @@ import "./home.scss"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="home">Home</div>
   )
 }
 
