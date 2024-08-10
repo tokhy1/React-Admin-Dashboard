1765c9cf0f4f1790a7cf3bb68f305a2e951abec0
mohamed21346587 <mohamed21346587@gmail.com>
2024-08-10 21:19:00 +0000

    Finished ChartBox

---
+++ src/components/chartBox/ChartBox.tsx
@@ -0,0 +1,60 @@
+import { Link } from "react-router-dom";
+import { Line, LineChart, ResponsiveContainer, Tooltip } from "recharts";
+import "./chartBox.scss";
+
+type Props = {
+  color: string;
+  icon: string;
+  title: string;
+  dataKey: string;
+  number: number | string;
+  percentage: number;
+  chartData: object[];
+};
+
+const ChartBox = (props: Props) => {
+  return (
+    <div className="chart-box">
+      <div className="box-info">
+        <div className="title">
+          <img src={props.icon} alt="" />
+          <span>{props.title}</span>
+        </div>
+        <h1>{props.number}</h1>
+        <Link to="/" style={{ color: props.color }}>
+          View all
+        </Link>
+      </div>
+      <div className="chart-info">
+        <div className="chart">
+          <ResponsiveContainer width="99%" height="100%">
+            <LineChart data={props.chartData}>
+              <Tooltip
+                contentStyle={{ background: "transparent", border: "none" }}
+                labelStyle={{ display: "none" }}
+                position={{ x: -20, y: 50 }}
+              />
+              <Line
+                type="monotone"
+                dataKey={props.dataKey}
+                stroke={props.color}
+                strokeWidth={2}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+        <div className="texts">
+          <span
+            className="percentage"
+            style={{ color: props.percentage < 0 ? "tomato" : "lightgreen" }}
+          >
+            {props.percentage}%
+          </span>
+          <span className="duration">this month</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChartBox;
