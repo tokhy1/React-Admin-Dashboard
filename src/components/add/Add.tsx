a56745f4312766a49cbb852827fa3c842e336468
mohamed21346587 <mohamed21346587@gmail.com>
2024-08-17 15:09:48 +0000

    Finished Add Modal

---
+++ src/components/add/Add.tsx
@@ -0,0 +1,43 @@
+import { GridColDef } from "@mui/x-data-grid";
+import "./add.scss";
+
+type Props = {
+  slug: string;
+  columns: GridColDef[];
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const Add = (props: Props) => {
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // make api request..
+    props.setOpen(false);
+  };
+
+  return (
+    <div className="add">
+      <div className="modal">
+        <span className="close" onClick={() => props.setOpen(false)}>
+          x
+        </span>
+        <h1>Add new {props.slug}</h1>
+        <form onSubmit={handleSubmit}>
+          {props.columns
+            .filter((item) => item.field !== "id" && item.field !== "img")
+            .map((column) => {
+              return (
+                <div className="item">
+                  <label>{column.headerName}</label>
+                  <input type={column.type} placeholder={column.field} />
+                </div>
+              );
+            })}
+            <button>Send</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Add;
--- src/pages/Users/Users.tsx
+++ src/pages/Users/Users.tsx
@@ -2,6 +2,8 @@ import { GridColDef } from "@mui/x-data-grid";
 import DataTable from "../../components/dataTable/DataTable";
 import { userRows } from "../../data";
 import "./users.scss";
+import { useState } from "react";
+import Add from "../../components/add/Add";
 
 const columns: GridColDef[] = [
   { field: "id", headerName: "ID", width: 90 },
@@ -51,16 +53,17 @@ const columns: GridColDef[] = [
   },
 ];
 
-
-
 const Users = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <div className="users">
       <div className="info">
         <h1>Users</h1>
-        <button>Add New User</button>
+        <button onClick={() => setOpen(true)}>Add New User</button>
       </div>
       <DataTable slug={"users"} rows={userRows} columns={columns} />
+      {open && <Add setOpen={setOpen} slug={"user"} columns={columns} />}
     </div>
   );
 };
